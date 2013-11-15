490763e4c83912d030d7569d013dca53242538d5
cumessage <cumessage@gmail.com>
2013-11-15 19:46:22 +0800

    db.sql

---
+++ clockgame-service-doc/Dump20131115.sql
@@ -0,0 +1,141 @@
+CREATE DATABASE  IF NOT EXISTS `clockgame` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `clockgame`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: clockgame
+-- ------------------------------------------------------
+-- Server version	5.6.0-m4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `friend`
+--
+
+DROP TABLE IF EXISTS `friend`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `friend` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` bigint(20) NOT NULL,
+  `friendid` bigint(20) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `createtime` bigint(20) NOT NULL DEFAULT '0',
+  `varifytime` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friend`
+--
+
+LOCK TABLES `friend` WRITE;
+/*!40000 ALTER TABLE `friend` DISABLE KEYS */;
+INSERT INTO `friend` VALUES (1,4,5,1,1384483984652,1384484227002),(2,5,4,1,1384484227035,1384484227002),(3,4,6,1,1384485260608,1384487066997),(4,5,6,1,1384485263941,1384487063720),(5,6,5,1,1384487063736,1384487063720),(6,6,4,1,1384487067010,1384487066997);
+/*!40000 ALTER TABLE `friend` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `game`
+--
+
+DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `playtime` bigint(20) NOT NULL,
+  `creatorid` bigint(20) NOT NULL,
+  `templateid` int(11) NOT NULL,
+  `createtime` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game`
+--
+
+LOCK TABLES `game` WRITE;
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+INSERT INTO `game` VALUES (7,1385511283000,4,1,1384512514630);
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `game_member`
+--
+
+DROP TABLE IF EXISTS `game_member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game_member` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gameid` bigint(20) NOT NULL,
+  `userid` bigint(20) NOT NULL,
+  `createtime` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game_member`
+--
+
+LOCK TABLES `game_member` WRITE;
+/*!40000 ALTER TABLE `game_member` DISABLE KEYS */;
+INSERT INTO `game_member` VALUES (3,7,4,1384512514658);
+/*!40000 ALTER TABLE `game_member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL DEFAULT '',
+  `isclosed` tinyint(4) NOT NULL,
+  `createtime` bigint(20) NOT NULL,
+  `lastupdate` bigint(20) NOT NULL,
+  `lastlogin` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'xxxx@sx.xx','1122','',0,1384411335328,1384411335328,1384423548632),(2,'xxxxxx@sx.xx','1122','',0,1384421814005,1384421814005,0),(3,'xxxxxxx@sx.xx','1122','',0,1384421859006,1384421859006,0),(4,'deacon.cu@gmail.com','11110000','deacon',0,1384422650331,1384425554428,1384424337699),(5,'kylin11@sina.com','11110000','kylin',0,1384483592423,1384483620196,0),(6,'emilycucu@126.com','11110000','emily',0,1384485243913,1384488162777,0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-11-15 19:45:23
